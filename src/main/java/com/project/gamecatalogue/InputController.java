aa4ebfb87230efeb5522dfba21e915945ce07634
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-10-11 11:22:40 +0300

    creating new window. Closing old window:

--- src/main/java/com/project/gamecatalogue/InputController.java
+++ src/main/java/com/project/gamecatalogue/InputController.java
@@ -1,15 +1,22 @@
 package com.project.gamecatalogue;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class InputController implements Initializable {
     @FXML private ChoiceBox<Genre> genre;
+    @FXML private Button searchButton;
     @FXML private TextField gameName;
 
     @Override
@@ -18,7 +25,23 @@ public class InputController implements Initializable {
     }
 
     @FXML
-    protected void onSearchButtonClick() {
-        System.out.println("Hello, World!");
+    protected void openSearchWindow() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GameCatalogue.class.getResource("output.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Stage stage = new Stage();
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Game Catalogue");
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+
+        closeWindow();
+    }
+
+    private void closeWindow() {
+        Stage stage = (Stage) searchButton.getScene().getWindow();
+        stage.close();
     }
 }
---
+++ src/main/java/com/project/gamecatalogue/OutputController.java
@@ -0,0 +1,5 @@
+package com.project.gamecatalogue;
+
+public class OutputController {
+
+}
