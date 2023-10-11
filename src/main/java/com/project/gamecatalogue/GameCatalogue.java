da43bf10e5c5ce94bf0b333890f0f1fc29aa0570
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-10-11 10:57:52 +0300

    Input controller

--- src/main/java/com/project/gamecatalogue/GameCatalogue.java
+++ src/main/java/com/project/gamecatalogue/GameCatalogue.java
@@ -7,13 +7,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class GameCatalogue extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(GameCatalogue.class.getResource("input.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Game Catalogue");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
---
+++ src/main/java/com/project/gamecatalogue/InputController.java
@@ -0,0 +1,11 @@
+package com.project.gamecatalogue;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class InputController {
+    @FXML
+    protected void onSearchButtonClick() {
+        System.out.println("Hello, World!");
+    }
+}
